cadc3ea4f7e78c7e54bcbc1cc7d2eba112b55a47
rosetanya898 <75381362+rosetanya898@users.noreply.github.com>
2021-05-17 01:37:46 +0530

    Update intput_text_field.dart

--- lib/screen/signin/widget/intput_text_field.dart
+++ lib/screen/signin/widget/intput_text_field.dart
@@ -2,45 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:mirraura/constant.dart';
 
 class InputTextFeild extends StatelessWidget {
-   const InputTextFeild({Key? key, required this.label,required this.onChange,this.password=false,}) : super(key: key);
-   final String label;
-   final Function onChange;
-   final bool password;
-               @override
-               Widget buildAppBar(BuildContext context){
-               return TextField(
-                 obscureText: password,
-                 onChanged: onChange(),
-                 cursorColor: Colors.grey,
-                 decoration: InputDecoration(
-                   labelText: label,
-                   labelStyle: TextStyle(color: Colors.grey,),
-                   border: UnderlineInputBorder(
-                     borderSide: BorderSide(
-                       color: spPrimaryColor,
-                       width:2,
-                     ),
-                   ),
-                   focusedBorder:  UnderlineInputBorder(
-                     borderSide: BorderSide(
-                       color: spPrimaryColor,
-                       width:2,
-                     ),
-                   ),
-                   enabledBorder:  UnderlineInputBorder(
-                     borderSide: BorderSide(
-                       color: Colors.grey,
-                       width:0.5,
-                     ),
-                   ),
-                 ),
-               );
-            
-          
-  }
+ final String label;
+ final Function onChange;
+ final bool password;
+  const InputTextFeild({
+    Key? key, required this.label, required this.onChange,  this.password=false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }}
+    return TextField(
+            obscureText: password,
+            onChanged: onChange(),
+            cursorColor: Colors.grey,
+            decoration: InputDecoration(
+              labelText: label,
+              labelStyle: TextStyle(color: Colors.grey,),
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: spPrimaryColor,
+                  width:2,
+                ),
+              ),
+              focusedBorder:  UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: spPrimaryColor,
+                  width:2,
+                ),
+              ),
+              enabledBorder:  UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.grey,
+                  width:0.5,
+                ),
+              ),
+            ),
+          );
+  }
+}
