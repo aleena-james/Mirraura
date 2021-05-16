6fc21c15ff944fd2dd6099d51ff956433f1168f3
Tanya Rose <rosetanya898@gmail.com>
2021-05-16 19:56:21 +0530

    sign up

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color spBackgroundColor= Color(0xFFFFFFFF);
+Color spPrimaryColor = Color(0xFFB98068);
+Color spPrimaryTextColor = Color(0xFF8C746A);
+Color spSecondTextColor = Color(0xFFB98068);
+Color spSecondColor = Color(0xFFC28E79);
+Color spTitleTextColor = Color(0xFF2D140D);
