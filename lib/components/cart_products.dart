63db304378c5da4b3d6cff65bebf5a74b033ddf1
rosetanya898 <75381362+rosetanya898@users.noreply.github.com>
2021-05-21 12:53:59 +0530

    Update cart_products.dart

--- lib/components/cart_products.dart
+++ lib/components/cart_products.dart
@@ -35,7 +35,6 @@ class _Cart_productsState extends State<Cart_products> {
             cart_prod_name: Product_on_the_cart[index]["name"],
             cart_prod_color: Product_on_the_cart[index]["color"],
             cart_prod_size: Product_on_the_cart[index]["size"],
-            cart_prod_qty: Product_on_the_cart[index]["siza"],
             cart_prod_price: Product_on_the_cart[index]["price"],
             cart_prod_picture: Product_on_the_cart[index]["picture"],
           );
@@ -43,29 +42,34 @@ class _Cart_productsState extends State<Cart_products> {
   }
 }
 
-class Singlr_cart_product extends StatelessWidget {
+class Singlr_cart_product extends StatefulWidget {
   final cart_prod_name;
   final cart_prod_picture;
   final cart_prod_price;
   final cart_prod_size;
   final cart_prod_color;
-  final cart_prod_qty;
 
   Singlr_cart_product({
     this.cart_prod_name,
     this.cart_prod_color,
     this.cart_prod_price,
     this.cart_prod_picture,
-    this.cart_prod_qty,
     this.cart_prod_size,
   });
+
+  @override
+  _Singlr_cart_productState createState() => _Singlr_cart_productState();
+}
+
+class _Singlr_cart_productState extends State<Singlr_cart_product> {
+  int cart_prod_qty = 1;
 
   @override
   Widget build(BuildContext context) {
     return Card(
         child: ListTile(
-      leading: new Image.asset(cart_prod_picture, width: 80, height: 80),
-      title: new Text(cart_prod_name),
+      leading: new Image.asset(widget.cart_prod_picture, width: 80, height: 80),
+      title: new Text(widget.cart_prod_name),
       subtitle: new Column(
         children: <Widget>[
           new Row(
@@ -76,7 +80,7 @@ class Singlr_cart_product extends StatelessWidget {
               ),
               Padding(
                 padding: const EdgeInsets.all(4.0),
-                child: new Text(cart_prod_size,
+                child: new Text(widget.cart_prod_size,
                     style: TextStyle(color: Colors.red)),
               ),
               new Padding(
@@ -85,28 +89,49 @@ class Singlr_cart_product extends StatelessWidget {
               ),
               Padding(
                 padding: const EdgeInsets.all(4.0),
-                child: new Text(cart_prod_color,
+                child: new Text(widget.cart_prod_color,
                     style: TextStyle(color: Colors.red)),
               ),
             ],
           ),
           new Container(
             alignment: Alignment.topLeft,
-            child: new Text("Rs ${cart_prod_price}",
-                style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.red)),
-          ),
+            child: Row(
+              children:[
+                 new Text("Rs ${widget.cart_prod_price}",
+                  style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red)),
+                       new Row(
+                        children: <Widget>[
+                         Padding(padding: const EdgeInsets.only(left:70.0),),
+                         new IconButton(icon: Icon(Icons.arrow_drop_up), onPressed: () {
+                           setState(() {
+                             cart_prod_qty += 1;
+                           });
+                         }),
+                         new Text("$cart_prod_qty"),
+                         new IconButton(icon: Icon(Icons.arrow_drop_down), onPressed: () {
+                           setState(() {
+                             if(cart_prod_qty>1)
+                             cart_prod_qty -= 1;
+                           });
+                         })
+      ],
+      ),
         ],
       ),
-    //  trailing: new Column(
+       ), ], ),
+          ),
+         
+    // trailing: new Column(
      //   children: <Widget>[
-       //   new IconButton(icon: Icon(Icons.arrow_drop_up), onPressed: () {}),
-         // new Text("$cart_prod_qty"),
-         // new IconButton(icon: Icon(Icons.arrow_drop_down), onPressed: () {})
-       // ],
-     // ),
-    ));
+      //    new IconButton(icon: Icon(Icons.arrow_drop_up), onPressed: () {}),
+       //   new Text("$cart_prod_qty"),
+       //  new IconButton(icon: Icon(Icons.arrow_drop_down), onPressed: () {})
+    //  ],
+    //  ),
+    );
   }
 }
